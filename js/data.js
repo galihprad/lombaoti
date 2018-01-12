d035c91ed719bfee1db90f6a866b989974b4bd63
Galih Pradipto Wisnujati <galih.p.w.j@gmail.com>
2018-01-12 16:33:39 +0700

    Update data.js
    findit

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
-lomba = ["TELKOM HACKATHON 2018", 
+lomba = ["FIND IT UGM",
+         "TELKOM HACKATHON 2018", 
          "MAGE ITS", 
          "SAYEMBARA DESAIN LOGO KBI", 
          "AAJI HACKATHON",
@@ -8,7 +9,8 @@ lomba = ["TELKOM HACKATHON 2018",
 
 
 
-tgl = ["February, 11, 2018",
+tgl = ["March, 5, 2018",
+         "February, 11, 2018",
        "January, 18, 2018",
        "January, 15, 2018",
        "January, 19, 2018",
@@ -17,7 +19,8 @@ tgl = ["February, 11, 2018",
        "December, 31, 2017"
     ];
 
-tag = ["<div class='chip yellow lighten-4'> hackathon </div>",
+tag = ["<div class='chip brown lighten-4'> business IT case </div><div class='chip teal lighten-4'> apps </div><div class='chip'> e-gov </div><div class='chip lime lighten-4'> UX </div><div class='chip orange lighten-4'> GameDev </div>",
+         "<div class='chip yellow lighten-4'> hackathon </div>",
         "<div class='chip teal lighten-4'> apps </div><div class='chip orange lighten-4'> GameDev </div><div class='chip'> IoT </div>",
         "<div class='chip green lighten-4'> multimedia </div>",
         "<div class='chip yellow lighten-4'> hackathon </div>",
@@ -25,7 +28,8 @@ tag = ["<div class='chip yellow lighten-4'> hackathon </div>",
       "<div class='chip teal lighten-4'> apps </div>",
       "<div class='chip teal lighten-4'> apps </div><div class='chip brown lighten-4'> business plan </div><div class='chip purple lighten-4'> CP </div><div class='chip orange lighten-4'> GameDev </div>"];
 
-link = ["https://expose.uzone.id/hackathon",
+link = ["http://finditugm.com/",
+         "https://expose.uzone.id/hackathon",
         "http://mage.telematics.its.ac.id/",
  "http://badanbahasa.kemdikbud.go.id/lamanbahasa/berita/2490/sayembara-desain-logo-kongres-bahasa-indonesia-kbi",
         "https://www.purwadhika.com/aaji-hackathon-2018",
@@ -35,7 +39,8 @@ link = ["https://expose.uzone.id/hackathon",
 
 
 
-imag = ["telkom.jpeg",
+imag = ["findit.jpg",
+         "telkom.jpeg",
         "mage.jpg",
         "kbi.jpg",
         "aaji.JPG",
