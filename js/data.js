3efb7465d01d85ca6bca67e05372f5bc2c817991
Galih Pradipto Wisnujati <galih.p.w.j@gmail.com>
2018-01-13 20:52:35 +0700

    Update data.js
    dinacom

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
-lomba = ["FIND IT UGM",
+lomba = ["DINACOM 2018",
+         "FIND IT UGM",
          "TELKOM HACKATHON 2018", 
          "MAGE ITS", 
          "SAYEMBARA DESAIN LOGO KBI", 
@@ -9,7 +10,8 @@ lomba = ["FIND IT UGM",
 
 
 
-tgl = ["March, 5, 2018",
+tgl = ["February, 10, 2018",
+         "March, 5, 2018",
          "February, 11, 2018",
        "January, 18, 2018",
        "January, 15, 2018",
@@ -19,7 +21,8 @@ tgl = ["March, 5, 2018",
        "December, 31, 2017"
     ];
 
-tag = ["<div class='chip brown lighten-4'> business IT case </div><div class='chip cyan lighten-4'> data mining </div><div class='chip teal lighten-4'> apps </div><div class='chip'> e-gov </div><div class='chip lime lighten-4'> UX </div><div class='chip orange lighten-4'> GameDev </div>",
+tag = ["<div class='chip teal lighten-4'> apps </div><div class='chip pink lighten-4'> web </div>",
+         "<div class='chip brown lighten-4'> business IT case </div><div class='chip cyan lighten-4'> data mining </div><div class='chip teal lighten-4'> apps </div><div class='chip'> e-gov </div><div class='chip lime lighten-4'> UX </div><div class='chip orange lighten-4'> GameDev </div>",
          "<div class='chip yellow lighten-4'> hackathon </div>",
         "<div class='chip teal lighten-4'> apps </div><div class='chip orange lighten-4'> GameDev </div><div class='chip'> IoT </div>",
         "<div class='chip green lighten-4'> multimedia </div>",
@@ -28,7 +31,8 @@ tag = ["<div class='chip brown lighten-4'> business IT case </div><div class='ch
       "<div class='chip teal lighten-4'> apps </div>",
       "<div class='chip teal lighten-4'> apps </div><div class='chip brown lighten-4'> business plan </div><div class='chip purple lighten-4'> CP </div><div class='chip orange lighten-4'> GameDev </div>"];
 
-link = ["http://finditugm.com/",
+link = ["http://dinacom.dinus.ac.id/",
+         "http://finditugm.com/",
          "https://expose.uzone.id/hackathon",
         "http://mage.telematics.its.ac.id/",
  "http://badanbahasa.kemdikbud.go.id/lamanbahasa/berita/2490/sayembara-desain-logo-kongres-bahasa-indonesia-kbi",
@@ -39,7 +43,8 @@ link = ["http://finditugm.com/",
 
 
 
-imag = ["findit.jpg",
+imag = ["dinacom.jpg",
+         "findit.jpg",
          "telkom.jpeg",
         "mage.jpg",
         "kbi.jpg",
